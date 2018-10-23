41fdc674eab68d7ef9a1e2563b1494a6fb398f08
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-10-23 23:00:36 +0800

    Add Xcode proj

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Sean Lim on 23/10/18.
+//  Copyright © 2018 Heka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
