b71a8069285e7d436ea36f6a5de5be96082499d3
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-11-12 10:47:06 +0800

    Update APIManger JSON response

--- Demo/Utility/APIManager.swift
+++ Demo/Utility/APIManager.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class APIManager {
 	static let shared = APIManager()
@@ -25,21 +26,46 @@ class APIManager {
 	func registerUser() {
 		let params: Parameters = [
 			"data": [
-				"name": userName,
-				"uid": userNric,
+				"name": userName ?? "",
+				"uid": userNric ?? "",
 				"nrics": ["1", "2"]
 			]
 		]
 		print("API: Register user with params... \(params)")
 		
-		self.request(path: .user, params: params, method: .post) {
+		self.request(path: .user, params: params, method: .post) { (succ, res) in
+			
 		}
 	}
 	
-	private func request(path: APIRoute, params: Parameters, method: HTTPMethod, _ callback: @escaping ()->Void) {
-		Alamofire.request(baseURL + path.rawValue, method: method, parameters: params, encoding: JSONEncoding.default, headers: [:]).response { dataResponse in
-			print("API: Server responded with status code \(dataResponse.response?.statusCode)")
+	func verifyNRIC(nric: String ) {
+		let params: Parameters = [
+			"queryId": nric
+		]
+		self.request(path: .user, params: params, method: .get) { (succ, res) in
+			
 		}
+	}
+	
+	private func request(path: APIRoute,
+						 params: Parameters,
+						 method: HTTPMethod,
+						 _ callback: @escaping (_ success: Bool, _ response: JSON?) -> Void) {
+		Alamofire
+			.request(baseURL + path.rawValue,
+						  method: method,
+						  parameters: params,
+						  encoding: JSONEncoding.default,
+						  headers:  self.defaultHeaders)
+			.response { dataResponse in
+				if let response = dataResponse.response, let data = dataResponse.data  {
+					print("API: Server responded with status code \(response.statusCode)")
+					let jsonData = try! JSON(data: data)
+					callback(true, jsonData)
+				} else {
+					callback(false, nil)
+				}
+			}
 	}
 
 }
