a69febf53da514b0d21061f133184263916c09eb
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-11-14 11:40:19 +0800

    Add barcode util

--- Demo/Utility/BarcodeGenerator.swift
+++ Demo/Utility/BarcodeGenerator.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+class Barcode {
+	
+	class func fromString(string : String) -> UIImage? {
+		
+		let data = string.data(using: .ascii)
+		let filter = CIFilter(name: "CICode128BarcodeGenerator")
+		filter?.setValue(data, forKey: "inputMessage")
+		
+		return UIImage(ciImage: (filter?.outputImage)!)
+	}
+	
+}
